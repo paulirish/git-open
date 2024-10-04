2a66fcd7e3583bc934843bad4af430ac2ffcd859
Rob Loach <RobLoach@users.noreply.github.com>
2024-10-03 21:17:24 -0400

    Add oh-my-bash support (#213)

---
+++ git-open.plugin.sh
@@ -0,0 +1,6 @@
+# This plugin is MIT licensed to match the git-open license.
+#
+# Make git-open easy to install and keep up to date if you're using a
+# Bash framework like oh-my-bash.
+
+export PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )":${PATH}
